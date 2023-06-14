ff64a0a0417819019cc83809e2f6ed4d42ff1cd0
Hans Wang <hansmikaa@live.cn>
2023-06-14 01:30:04 -0400

    add add new url function, add generate short URL ID function

--- express_server.js
+++ express_server.js
@@ -9,14 +9,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+	let shortURLID = '';
+  let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 6; i++) {
+		shortURLID += arr[(Math.floor(Math.random() * 6))];
+	}
+	return shortURLID;
+}
+
+randomStr(20, '12345abcde');
+
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {

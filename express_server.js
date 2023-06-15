968351f956bc88cdb027aeafc5a7229a673a8c9a
Hans Wang <hansmikaa@live.cn>
2023-06-15 14:03:27 -0400

    add clearcookie function when log in username exist

--- express_server.js
+++ express_server.js
@@ -82,6 +82,12 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 })
 
+//Logout route
+app.post('/logout', (req, res) => {
+  res.clearCookie("username");
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Tinyapp listening on port ${PORT}!`);
 });
